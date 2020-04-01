1b43c62af90bccba5232072f04e6c7adc627fd8c
jimmymetts <jimmymetts@bellsouth.net>
2020-04-01 18:12:16 -0500

    added getRecipe to App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import './App.css';
 import Form from './components/Form';
 
 class App extends Component {
+  getRecipe = () {
+    console.log("Working!!!")
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form/>
       </div>
     );
   }
